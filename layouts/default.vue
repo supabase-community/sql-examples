e46e99bca7eb05cbbc76108d851fa7ad45b49d29
zernonia <59365435+zernonia@users.noreply.github.com>
2021-09-17 12:10:47 +0800

    Resize observer for Modal

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,3 +15,27 @@
     <Footer></Footer>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from "@nuxtjs/composition-api";
+import { useWindowSize, useResizeObserver } from "@vueuse/core";
+
+export default defineComponent({
+  setup() {
+    const { width } = useWindowSize();
+
+    onMounted(() => {
+      useResizeObserver(document.documentElement, (entries) => {
+        const entry = entries[0];
+        const box = entry.contentRect;
+        document.documentElement.style.setProperty(
+          "--scrollbarWidth",
+          width.value - box.width + "px"
+        );
+      });
+    });
+
+    return {};
+  },
+});
+</script>
