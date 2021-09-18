ca6226eb43142ec8d210cbafa1032bda7c840d08
zernonia <59365435+zernonia@users.noreply.github.com>
2021-09-19 00:31:52 +0800

    update: change assets to static. fix linkpreview

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,21 +3,21 @@
     <div class="text-sm text-center flex space-x-1">
       <LinkPreview
         target="_blank"
-        :image="'./assets/supabase-schema.png'"
+        :image="'./supabase-schema.png'"
         href="https://supabase-schema.vercel.app/"
         >Supabase Schema</LinkPreview
       >
       <span> | </span>
       <LinkPreview
         target="_blank"
-        :image="'./assets/supabase-realtime.png'"
+        :image="'./supabase-realtime.png'"
         href="http://supabase-realtime-nextconf-clone.vercel.app/"
         >Supabase Realtime Playground</LinkPreview
       >
       <span> | </span>
       <LinkPreview
         href="https://www.madewithsupabase.com/"
-        :image="'./assets/made-with-supabase.png'"
+        :image="'./made-with-supabase.png'"
         >Made With Supabase</LinkPreview
       >
     </div>
